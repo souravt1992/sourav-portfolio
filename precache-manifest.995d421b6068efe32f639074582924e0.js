171166e559d12d1b1b024ca8dee055d8dd263baf
sourav <tsourav1992@gmail.com>
2020-01-03 09:03:57 +0530

    Updates

---
+++ precache-manifest.995d421b6068efe32f639074582924e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b49812d7c61d3a6dd0335341e497339",
+    "url": "/sourav-portfolio/index.html"
+  },
+  {
+    "revision": "a6805906cf625529b0b7",
+    "url": "/sourav-portfolio/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2b2d4e9cd1d965c177aa",
+    "url": "/sourav-portfolio/static/js/2.fdb1ac40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sourav-portfolio/static/js/2.fdb1ac40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6805906cf625529b0b7",
+    "url": "/sourav-portfolio/static/js/main.067033e2.chunk.js"
+  },
+  {
+    "revision": "d3a4b2900f99d07cdc99",
+    "url": "/sourav-portfolio/static/js/runtime-main.52893d8b.js"
+  },
+  {
+    "revision": "d5da7c4fc0fc259a843d9ff1bf0b956d",
+    "url": "/sourav-portfolio/static/media/partner-1.d5da7c4f.png"
+  },
+  {
+    "revision": "2a80864d23def03e16f99a07ad6c1835",
+    "url": "/sourav-portfolio/static/media/partner-2.2a80864d.png"
+  },
+  {
+    "revision": "c6a244ae0dd1f4d0ce33eeb9f4c3e97a",
+    "url": "/sourav-portfolio/static/media/partner-3.c6a244ae.png"
+  },
+  {
+    "revision": "0001f801abd9d4a92edf28109ce25497",
+    "url": "/sourav-portfolio/static/media/partner-4.0001f801.png"
+  },
+  {
+    "revision": "965ae31b55378078ffb38c0f9790e2e1",
+    "url": "/sourav-portfolio/static/media/partner-5.965ae31b.png"
+  }
+]);
